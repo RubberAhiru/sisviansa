07225fa559eeb04036f986824161ee55c9347f6c
Allison Flores <aallisonfloo@gmail.com>
2023-11-06 04:21:44 -0300

    separo el carrito a un js aparte

--- presentacion/js/main.js
+++ presentacion/js/main.js
@@ -65,20 +65,3 @@ function main(){
 
 };
 
-//Carrito:
-$(document).ready(function () {
-
-    function showPopup(){
-        $('.pop-up').addClass('show');
-        $('.pop-up-wrap').addClass('show');
-    }
-
-    $("#close").click(function(){
-        $('.pop-up').removeClass('show');
-        $('.pop-up-wrap').removeClass('show');
-    });
-
-    $(".btn-abrir").click(showPopup); //cuando toque el carrito se abre
-
-});
-
