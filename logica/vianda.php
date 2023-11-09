b1879696f228540c73bdccbc81ab1cd61a34d28f
neodev <techdev7@protonmail.com>
2023-11-09 04:39:50 -0300

    Se movio la funcion listarTodas() a viandaDAO php

--- logica/vianda.php
+++ logica/vianda.php
@@ -1,6 +1,6 @@
 <?php
 
-include '../persistencia/conexion.php';
+//include '../persistencia/conexion.php';
 
 
 class Vianda {
@@ -11,12 +11,12 @@ class Vianda {
     protected $precio;
     protected $contenido;
     protected $imagen;
-    protected $conex;
+    //protected $conex;
 
     //Constructor
     public function __construct()
     {
-        $this->conex = BaseDeDatos::conectar();
+        //$this->conex = BaseDeDatos::conectar();
     }
 
     //Setters
@@ -57,33 +57,6 @@ class Vianda {
     }
     public function getImagen(){
         return $this->imagen;
-    }
-
-    public function guardar(){
-        
-    }
-
-    public function listarTodas(){
-        //Consulta todos los datos en la tabla vianda
-        //con Prepared Statement (Declaracion Preparada)
-        $stmt = $this->conex ->prepare("SELECT * FROM vianda ORDER BY nombre");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        
-        //Crea array de viandas 
-        //y lo llena con todos los elementos que se obtuvieron de la consulta
-        $viandas = [];
-        while($row = $result->fetch_assoc()){
-            $viandas[] = $row;
-        }
-
-        //Cerrar instancia
-        $stmt->close();
-        //Cerrar conexion
-        $this->conex ->close();
-
-        //var_dump($viandas);
-        return $viandas;
     }
 
 }
