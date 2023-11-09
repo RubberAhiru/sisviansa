f1a330a434d3f77de0a070a755aeaee757b0b517
neodev <techdev7@protonmail.com>
2023-11-09 04:45:58 -0300

    Se agrego funcion de Inicio de sesion

--- persistencia/clienteDAO.php
+++ persistencia/clienteDAO.php
@@ -9,4 +9,21 @@ class clienteDAO
 
     }
 
+    public function login($u, $c){
+    //Funcion de login
+    //busca al usuario en la base de datos y comprueba si la contrasenia ingresada es correcta
+        //prepara una declaracion de consulta SELECT
+        $stmt = $this->conex->prepare("SELECT * FROM cliente WHERE email = ?");
+        $stmt->bind_param("s", $u);
+        //Ejecuta declaracion para hacer consulta
+        $stmt->execute();
+        //Guarda el resultado
+        $result = $stmt->get_result();
+        $usuario = $result->fetch_object();
+        //Verifica la contrasenia 
+        $verify = password_verify($c, $usuario->contrasenia);
+        //retorna variable verify como true o false
+        return $verify;
+    }
+
 }
