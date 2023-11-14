294f3940088b03f24f2764f983a387dfd182f98f
neodev <techdev7@protonmail.com>
2023-11-13 22:19:41 -0300

    Case de logout agregado

--- logica/controladorCliente.php
+++ logica/controladorCliente.php
@@ -7,22 +7,18 @@ include '../persistencia/dao/clienteDAO.php';
 include '../persistencia/dao/clientePersonaDAO.php';
 include '../persistencia/dao/clienteEmpresaDAO.php';
 
-if($_POST){
-    //Tipos de datos que cliente-persona y cliente-empresa tienen en comun
-    //nroCliente Es un dato autoincrement en la bd
-    $email = $_POST['email'];
-    $cont = $_POST['contrasenia'];
-    $tel = $_POST['telefono'];
-    $calle = $_POST['calle'];
-    $nCalle = $_POST['numerocalle'];
-    $barr = $_POST['barrio'];
-
     
-    switch ($_POST['accion']) {
+    switch ($_REQUEST['accion']) {
 
-        case ($_POST['accion']) == 'insert-cliente-persona':
+        case ('insert-cliente-persona'):
             //////////////////////////////insert para cliente persona/////////////////////////////////////
-            //tipos de datos de cliente-persona
+            $email = $_POST['email'];
+            $cont = $_POST['contrasenia'];
+            $tel = $_POST['telefono'];
+            $calle = $_POST['calle'];
+            $nCalle = $_POST['numerocalle'];
+            $barr = $_POST['barrio'];
+
             $nom = $_POST['nombre'];
             $ape = $_POST['apellido'];
             $nroDoc = $_POST['nrodocumento'];
@@ -44,7 +40,7 @@ if($_POST){
             //VALIDACION
             //comprueba que todos los campos esten llenos y si los datos ingresados son validos
             if ($camposLlenos && $datosValidos) {
-                echo "persona";
+           
                 //crea objeto Persona
                 $persona = new Persona();
                 //setea atributos al objeto
@@ -73,9 +69,15 @@ if($_POST){
 
             break;
 
-        case ($_POST['accion']) == 'insert-cliente-empresa':
+        case ('insert-cliente-empresa'):
             //////////////////////////////insert para cliente empresa/////////////////////////////////////
-            //tipos de datos de cliente-empresa
+            $email = $_POST['email'];
+            $cont = $_POST['contrasenia'];
+            $tel = $_POST['telefono'];
+            $calle = $_POST['calle'];
+            $nCalle = $_POST['numerocalle'];
+            $barr = $_POST['barrio'];
+            
             $rut = $_POST['rut'];
             $rSocial = $_POST['razon-social'];
 
@@ -96,8 +98,6 @@ if($_POST){
 
             //comprueba que todos los campos esten llenos
             if ($camposLlenos && $datosValidos) {
-
-                echo "empresa";
 
                 $empresa = new Empresa();
 
@@ -121,7 +121,7 @@ if($_POST){
             }
             break;
 
-        case ($_POST['accion']) == 'login':
+        case ('login'):
             //////////////////////////////Inicio de Sesión/////////////////////////////////////
             $usu = $_POST['usuario'];
             $con = $_POST['contrasenia'];
@@ -136,101 +136,13 @@ if($_POST){
             }
             header('Location: ../presentacion/index.html');
             break;
+
+        case ('cerrar-sesion'):
+
+            session_start();
+            session_destroy();
+            echo "lcdtm";
+            header('Location: ../presentacion/index.html');
+            break;
     }
 
-}
-
-
-
-/*
-if (($_POST['accion']) == 'insert-cliente-persona') {
-//////////////////////////////insert para cliente persona/////////////////////////////////////
-//tipos de datos de cliente-persona
-$nom = $_POST['nombre'];
-$ape = $_POST['apellido'];
-$nroDoc = $_POST['nrodocumento'];
-$tipoDoc = $_POST['tipodocumento'];
-$camposLlenos;
-//Verfica que cada campo no este vacio
-foreach ($_POST as $campos) {
-if (!empty(trim($campos))) {
-$camposLlenos = true;
-} else {
-$camposLlenos = false;
-}
-}
-//Guarda en variable datosValidos el resultado que da la invocacion de la funcion validaPersona (true o false)
-$datosValidos = validaPersona($email, $cont, $calle, $nCalle, $tel, $barr, $nom, $ape, $nroDoc, $tipoDoc);
-//VALIDACION
-//comprueba que todos los campos esten llenos y si los datos ingresados son validos
-if ($camposLlenos && $datosValidos) {
-//crea objeto Persona
-$persona = new Persona();
-//setea atributos al objeto
-$persona->setEmail($email);
-$persona->setContrasenia($cont);
-$persona->setTelefono($tel);
-$persona->setCalle($calle);
-$persona->setNumCalle($nCalle);
-$persona->setBarrio($barr);
-$persona->setNombre($nom);
-$persona->setApellido($ape);
-$persona->setNroDocumento($nroDoc);
-$persona->setTipoDocumento($tipoDoc);
-//crea objeto clientePersonaDAO
-$insertPersona = new clientePersonaDAO();
-//Invoca a la funcion guardar, para realizar los inserts a la base de datos
-$insertPersona->guardar( $persona->getEmail(), $persona->getContrasenia(), $persona->getTelefono(),
-$persona->getCalle(), $persona->getNumCalle(), $persona->getBarrio(), $persona->getNombre(),
-$persona->getApellido(), $persona->getNroDocumento(), $persona->getTipoDocumento() );
-}else{
-//echo "N";
-}
-} else if (($_POST['accion']) == 'insert-cliente-empresa') {
-//////////////////////////////insert para cliente empresa/////////////////////////////////////
-//tipos de datos de cliente-empresa
-$rut = $_POST['rut'];
-$rSocial = $_POST['razon-social'];
-$camposLlenos;
-foreach ($_POST as $campos) {
-if (!empty(trim($campos))) {
-$camposLlenos = true;
-} else {
-$camposLlenos = false;
-}
-}
-$datosValidos = validaEmpresa($email, $cont, $calle, $nCalle, $tel, $barr, $rut, $rSocial);
-//comprueba que todos los campos esten llenos
-if ($camposLlenos && $datosValidos) {
-echo "SI";
-$empresa = new Empresa();
-$empresa->setEmail($email);
-$empresa->setContrasenia($cont);
-$empresa->setTelefono($tel);
-$empresa->setCalle($calle);
-$empresa->setNumCalle($nCalle);
-$empresa->setBarrio($barr);
-$empresa->setRut($rut);
-$empresa->setRazonSocial($rSocial);
-$insertEmpresa = new clienteEmpresaDAO();
-$insertEmpresa->guardar(
-$empresa->getEmail(), $empresa->getContrasenia(), $empresa->getTelefono(), $empresa->getCalle(),
-$empresa->getNumCalle(), $empresa->getBarrio(), $empresa->getRut(), $empresa->getRazonSocial()
-);
-} else {
-//echo "NO";
-}
-} else if (($_POST['accion']) == 'login') {
-echo "login";
-$usu = $_POST['usuario'];
-$con = $_POST['contrasenia'];
-$clienteLogin = new clienteDAO();
-$login = $clienteLogin->login($usu, $con);
-if ($login) {
-session_start();
-//session_destroy();
-$_SESSION['usuario'] = $usu;
-var_dump($_SESSION);
-}
-}
-*/
