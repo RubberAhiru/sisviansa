2c203ff2f84724bb78a7a5ea44c06b658e9246dc
neodev <techdev7@protonmail.com>
2023-11-16 18:09:46 -0300

    Configuracion de conexion de base de datos para el servidor del ITI

--- persistencia/conexion.php
+++ persistencia/conexion.php
@@ -1,22 +1,26 @@
 <?php
 
-class BaseDeDatos
+
+class BaseDeDatos{
+
+    public static function conectar(){
+                        //(host, usuario, password, base de datos)
+        $db = new mysqli("192.168.2.209/phpmyadmin", "rubberahiru", "patitodehule", "rubber_ahiru");
+        $db->query("SET NAMES 'utf8'");
+        return $db;
+    }
+}
+
+/*class BaseDeDatos
 {
 
     public static function conectar()
-    { //introducir el password en el 3er parametro!!
+    { 
         //(host, usuario, password, base de datos)
         $db = new mysqli("localhost", "root", "", "Rubber_Ahiru");
         $db->query("SET NAMES 'utf8'");
         return $db;
     }
-}/*
-class BaseDeDatos{
-
-    public static function conectar(){ //introducir el password en el 3er parametro!!
-                        //(host, usuario, password, base de datos)
-        $db = new mysqli("192.168.2.209/phpmyadmin", "rubberahiru", "", "rubber_ahiru");
-        $db->query("SET NAMES 'utf8'");
-        return $db;
-    }
 }*/
+
+?>
